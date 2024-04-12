50524983caa2bbb96bc01aa1312e2bcb7e4c7a16
desokan <thefdmm@gmail.com>
2024-04-12 21:59:30 +0100

    created an array and a random message

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,21 @@
-fun main () {
-    val myArray = arrayOf("1", "2", "3", 9)
-    println(myArray[3])
-    println(myArray.size)
-    println(myArray[myArray.size - 1])
+fun main (args: Array<String>) {
+
+    val myNewArray: Array<Int> = arrayOf(1,2,3)
+    println(myNewArray[2])
+
+    val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
+    val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
+    val wordArray3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
+
+    val arraySize1 = wordArray1.size
+    val arraySize2 = wordArray2.size
+    val arraySize3 = wordArray3.size
+
+    val rand1 = (Math.random() * arraySize1).toInt()
+    val rand2 = (Math.random() * arraySize2).toInt()
+    val rand3 = (Math.random() * arraySize3).toInt()
+
+    val phrase = "${wordArray1[rand1]} ${wordArray2[rand2]} ${wordArray3[rand3]}"
+
+    println(phrase)
 }
